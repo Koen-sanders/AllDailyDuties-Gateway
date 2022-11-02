973bba729380c6df766e6adb8c564e700724e598
Koen-sanders <koensanders@outlook.com>
2022-11-02 23:53:30 +0100

    Added bearer token check to gateway endpoint

--- AllDailyDuties-Gateway/Program.cs
+++ AllDailyDuties-Gateway/Program.cs
@@ -1,3 +1,5 @@
+using AllDailyDuties_Gateway;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -9,6 +11,17 @@ builder.Configuration.AddJsonFile("Ocelot.json");
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = TokenService.CreateTokenValidationParameters();
+});
+
 builder.Services.AddOcelot();
 
 
---
+++ AllDailyDuties-Gateway/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace AllDailyDuties_Gateway
+{
+    public class TokenService
+    {
+        public static TokenValidationParameters CreateTokenValidationParameters()
+        {
+            var result = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = false,
+
+                SignatureValidator = delegate (string token, TokenValidationParameters parameters)
+                {
+                    var jwt = new JwtSecurityToken(token);
+                    return jwt;
+                },
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+            };
+            result.RequireSignedTokens = false;
+            return result;
+        }
+    }
+}
